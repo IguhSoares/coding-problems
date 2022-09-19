f3a675bc4c6677cf96dc4edf2bf805cff2b42e3a
iguhSoares <iguhsoares@gmail.com>
2022-09-19 11:07:25 -0300

    improved deserialize() time complexity from O(n^2) to O(n)

--- bin_tree_serialize.py
+++ bin_tree_serialize.py
@@ -32,46 +32,35 @@ def deserialize(s: str) -> Node:
     values: List[Any] = s.split(', ')
     root: Node = Node(int( values.pop(0) ), -1, -1)
     
-    while values != []:
+    queue = [root]
+    while queue != []:
+        node = queue.pop(0)
         val = values.pop(0)
-        
-        queue = [root]
-        while queue != []:
-            node = queue.pop(0)
-            if node == -1:
-                node = None if val == 'None' else Node(int(val), -1, -1)
-                break
-            elif node.left == -1:
-                node.left = None if val == 'None' else Node(int(val), -1, -1)
-                break
-            elif node.right == -1:
-                node.right = None if val == 'None' else Node(int(val), -1, -1)
-                break
 
-            if node != None:
-                if node.left != None:
-                    queue.append(node.left)
-                if node.right != None:
-                    queue.append(node.right)
+        if node == -1:
+            node = None if val == 'None' else Node(int(val), -1, -1)
+        else:
+            if node.left == -1:
+                node.left = None if val == 'None' else Node(int(val), -1, -1)
+            if node.right == -1:
+                val = values.pop(0)
+                node.right = None if val == 'None' else Node(int(val), -1, -1)
+        
+        if node != None:
+            if node.left != None:
+                queue.append(node.left)
+            if node.right != None:
+                queue.append(node.right)
 
     return root
 
 root = Node(16)
 array = [1, 7, 8, 32, 25, 21, 3, 64, 55, 43]
-# array = [15, 17]
 for n in array:
     root.append(n)
 
-print( serialize(root) )
-print('\n')
-print(serialize(  deserialize(serialize(root))  ))
-# tree =   deserialize('16, 1, 32, 0, 7, 25, None, None, None, None, None, None, None')  
-# queue = [tree]
-# while queue != []:
-#     node = queue.pop(0)
-#     print(f'Node: {node.value} => left: {  node.left.value if node.left not in [-1, None]  else node.left }')
-#     print(f'Node: {node.value} => right: {  node.right.value if node.right not in [-1, None] else node.right }')
-#     if type(node.left) == Node:
-#         queue.append(node.left)
-#     if type(node.right) == Node:
-#         queue.append(node.right)
+tree_to_str = serialize(root)
+print(f'Serialize:\n {type(tree_to_str)} -> {tree_to_str}\n')
+tree = deserialize(tree_to_str)
+print(f'Deserialize:\n{type(tree)}')
+tree.print_tree()
